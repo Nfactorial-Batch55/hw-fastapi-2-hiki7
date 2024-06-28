6fe2f453c5b441e3233820a8001f082e91706e09
hiki7 <rsaliyev03@gmail.com>
2024-06-28 14:22:12 +0500

    cars-id is solved

--- cars/app/main.py
+++ cars/app/main.py
@@ -20,4 +20,11 @@ def get_cars(page: int = 1, limit: int = 10):
         raise HTTPException(status_code=404, detail="No cars found")
     return {"cars": cars[start:end]}
 
+
+@app.get("/cars/{id}")
+def get_car(id: int):
+    for car in cars:
+        if car["id"] == id:
+            return car
+    raise HTTPException(status_code=404, detail="Not found")
 # (конец решения)
