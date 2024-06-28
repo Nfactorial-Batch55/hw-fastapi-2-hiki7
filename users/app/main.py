4291761e430909ec076b06191688815d984f9c32
hiki7 <rsaliyev03@gmail.com>
2024-06-28 15:40:37 +0500

    created get_user handler for displaying the information about one user

--- users/app/main.py
+++ users/app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Request, Response
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import PlainTextResponse
 
 from .users import create_users
 
@@ -23,4 +24,17 @@ def get_users(request: Request):
     })
 
 
+@app.get("/users/{id}")
+def get_user(request: Request, id: int):
+    my_user = None
+    for user in users:
+        if user['id'] == id:
+            my_user = user
+            break
+    if my_user is None:
+        return PlainTextResponse("Not found", status_code=404)
+    return templates.TemplateResponse("users/user.html", {
+        "request": request,
+        "my_user": my_user
+    })
 # (конец решения)
